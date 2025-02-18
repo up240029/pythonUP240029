ab5954e602226ad4e77a652cfcbac2f187ae4b5b
javier <up240029@alumnos.upa.edu.mx>
2025-02-18 09:03:29 -0600

    Avance del 3

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,4 +1,34 @@
-edad  = 19 
-Peso = 78.9
-complejo = 3 + 4j
+# Problemas 1, 2 y 3
+edad = 19
+Peso = 78.5 
+complex = 2+3j
+print(type(edad))
+print(type(Peso))
+print(type(complex))
+# Problema 4 
+base = float(input("ingrsa el valor de la base: "))
+altura = float(input("ingresa el valor de la altura: "))
+area = 0.5 * base * altura
+print ("El area del triangulo es: ", area )
+# Problema 5
+lado1 = float(input("ingresa el primer lado: "))
+lado2 = float(input("ingresa el segundo lado: "))
+lado3 = float(input("ingresa el tercer lado: "))
+perimetro = lado1 + lado2 + lado3 
+print ("El perimetro es: ",perimetro )
+#Programa 6
+longitud = float(input("ingresa la longitud: "))
+ancho = float(input(" ingresa el ancho: "))
+area = longitud * ancho
+perimetro = 2 * ( longitud + ancho )
+print( "El area es: ", area)
+print ( "El perimero es: ", perimetro)
+#Programa 7
+radio = float(input("ingresa el valor del radio: "))
+pi = 3.14
+area = pi * radio**2 
+perimetro = 2 * pi * radio
+print("El area del cireculo es: ",area)
+print("El perimertro es: " , perimetro)
+
 
