b9e844cd9bd7e10f3c440c1c0d7b2f5f76d4ff2c
javier <up240029@alumnos.upa.edu.mx>
2025-02-25 08:30:31 -0600

    Inicio dia 4

---
+++ dia 4.py
@@ -0,0 +1,16 @@
+#Programa 1 
+variable1 = "thirty "
+variable2 = "days " 
+variable3 = 'of '
+variable4 = 'phyton'
+print(variable1 + variable2 + variable3 + variable4)
+#Programa 2
+Primera = 'Coding '
+Segunda = 'for '
+Tercera = 'all'
+print(Primera + Segunda + Tercera)
+# Programa 3
+company = "Coding for all"
+print(company)
+
+
