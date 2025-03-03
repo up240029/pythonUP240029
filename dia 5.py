450befcec81331aed3211f052851b4ad49ddf7fb
javier <up240029@alumnos.upa.edu.mx>
2025-03-02 19:30:42 -0600

    Inicio dia 5

---
+++ dia 5.py
@@ -0,0 +1,11 @@
+#Programa 1
+emptyList =[]
+print(len(emptyList))
+#Programa 2
+list = ['Azucar','Avena','Leche','Pan','Huevos']
+#Programa 3
+print(len(list))
+#Programa 4
+print(list[0])
+print(list[2])
+print(list[4])
