b473aacf2a8fd23ed80215b0656d43c623160d7e
javier <up240029@alumnos.upa.edu.mx>
2025-03-04 09:30:32 -0600

    avance dia 5

--- dia 5.py
+++ dia 5.py
@@ -9,3 +9,38 @@ print(len(list))
 print(list[0])
 print(list[2])
 print(list[4])
+#Programa 5
+mixedDataTypes = ['Javier', '19', '77.8', 'single', 'Hacienda gracias a dios no. 155']
+#Programa 6
+itCompanies =['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+#Programa 7
+print(itCompanies)
+#Programa 8
+print(len(itCompanies))
+#Programa 9
+print(itCompanies[0],itCompanies[3],itCompanies[6])
+#Programa 10
+print(itCompanies)
+itCompanies[0] = 'Meta'
+print(itCompanies)
+#Programa 11
+print(itCompanies)
+itCompanies.append('E-bay')
+print(itCompanies)
+#Programa 12
+itCompanies.insert(3, 'tesla')
+print(itCompanies)
+#Programa 13
+itCompanies[0] = itCompanies[0].upper()
+print(itCompanies)
+#Programa 14
+print('#'.join(itCompanies))
+#Programa 15
+doesExist = 'tesla' in itCompanies
+print(doesExist)
+#Programa 16
+itCompanies.sort()
+print(itCompanies)
+
+
+
