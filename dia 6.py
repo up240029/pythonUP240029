2b62085c7057bb9008e7f96ed901e0ccae572bc2
javier <up240029@alumnos.upa.edu.mx>
2025-03-10 21:03:37 -0600

    Empece esta madre

---
+++ dia 6.py
@@ -0,0 +1,15 @@
+#Programa 1
+emptyTuple= ()
+print(len(emptyTuple))
+#Programa 2
+hermanos = ('Santiago','Isai','Octavio')
+Hermanas = ('Sofia','Luna','Majo')
+print(hermanos)
+print(Hermanas)
+#Programa 3
+hermanes = hermanos + Hermanas
+#Programa 4
+print('Tengo {} hermanos'.format(len(hermanes)))
+#Programa 5
+miembrosFam = hermanes + ('Paco', 'Cony')
+print(miembrosFam)
