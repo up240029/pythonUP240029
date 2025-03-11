f6bd539070ccc0ad026fe92eb2e4bc242c1e13f1
javier <up240029@alumnos.upa.edu.mx>
2025-03-11 09:06:20 -0600

    Inicio dia 7

---
+++ dia 7.py
@@ -0,0 +1,17 @@
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+#Nivel 1
+#Programa 1
+print(len(it_companies))
+#Programa 2
+it_companies.add('Twiter')
+print(it_companies)
+#Programa 3
+it_companies.update(['Tesla', 'Space x'])
+print(it_companies)
+#Programa 4
+it_companies.remove('Google')
+print(it_companies)
+#Programa 5
