740dc55c757fe932aeaa41e4efd7ff4da5ef59d2
javier <up240029@alumnos.upa.edu.mx>
2025-03-12 09:25:53 -0600

    avance perron

--- dia 7.py
+++ dia 7.py
@@ -25,4 +25,18 @@ print(A.union(B))
 print(A.intersection(B))
 #3
 print(A.issubset(B))
+#4
+print(A.isdisjoint(B))
+#5
+print(A.union(B))
+print(B.union(A))
+#6
+print(A.symmetric_difference(B))
+#7
+del A
+#Nivel 3
+
+
+
+
 
