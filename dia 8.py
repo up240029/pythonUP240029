6737c591b65ea945cf6b9b442b795e537b939ce0
javier <up240029@alumnos.upa.edu.mx>
2025-03-13 11:44:51 -0600

    avance dia 8

--- dia 8.py
+++ dia 8.py
@@ -1,3 +1,28 @@
 # Programa 1
 dog = {}
 # Programa 2
+dog['nombre'] = 'candy'
+dog['color'] = 'cafe'
+dog['raza'] = 'labrador'
+dog['patas'] = '4'
+dog['edad'] = '5'
+print(dog)
+#Programa 3
+estudiantes = {
+    'Nombre': 'Javier',
+    'Apellido': 'Marquez',
+    'Genero': 'Masculino',
+    'Edad': '19',
+    'Estado civil': 'Soltero',
+    'Habilidades': ['Veloz','Inteligente','matematicas'],
+    'Pais': 'Mexico',
+    'Ciudad': 'Aguascalentes',
+    'Direccion': {
+        'Calle': 'Haciendas gracias a Dios no. 155',
+        'Colonia': 'Haciendas de Aguascalientes'
+    }
+
+    }
+#Programa 4
+print(len(estudiantes))
+#Programa 5
