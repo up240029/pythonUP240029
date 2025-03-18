c9e67428422e032395195298e45bbf4db3b49d42
Luis Anaya <luis.ernesto.anaya@upa.edu.mx>
2025-03-18 09:16:23 -0600

    Add review print statements to dia 6.py and dia 7.py

--- 06_Day/dia 6.py
+++ 06_Day/dia 6.py
@@ -35,3 +35,5 @@ del foodStuffTp
 nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
 print('Estonia' in nordic_countries)
 print('Iceland' in nordic_countries)
+
+print('Revisado')
