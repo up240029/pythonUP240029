0212e15f7c2cad5cffa1ba84bb8606e1091650d9
javier <up240029@alumnos.upa.edu.mx>
2025-03-19 10:41:05 -0600

    dia 9

--- 09_Day/dia 9.py
+++ 09_Day/dia 9.py
@@ -55,9 +55,39 @@ else:
     print('La temporada es verano')
 #3
 fruits = ['banana', 'orange', 'mango', 'lemon']
-fruta = input('Ingresa la fruta:')
-if fruta in fruits:
-    print('Esta fruta si existe en esta lista')
+newFruit = input('Inserta una fruta: ')
+if newFruit in fruits:
+    print('La fruta ya existe')
 else:
-    fruits.append(fruta)
-    print(fruits)
+    fruits.append(newFruit)
+    print('lista modificada ',fruits)
+#Nivel 3
+person={
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_marred': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+    }
+#1
+if 'skills' in person:
+    print(person['skills'] [len(person['skills'])//2])
+#2
+    if 'Python' in person['skills']:
+        print(person['skills'])
+# 
+if 'skills' in person:
+    hab = person['skills']
+    if 'JavaScrip'in hab and 'React'in hab:
+        print('He is a front end developer')
+    elif 'Node' in hab and 'Phyron'in hab and'MongoDB' in hab:
+        print('He is a backed developer')
+    elif 'React' in hab and 'Node' in hab and 'MongoDB' in hab:
+        print('He is a fullstack developer')
+    else:
+        print('unknown title')
