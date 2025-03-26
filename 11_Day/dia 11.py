8a03d7c0a506f06acac84351e9bec91d6a263268
javier <up240029@alumnos.upa.edu.mx>
2025-03-26 10:35:43 -0600

    inicio dia 11

---
+++ 11_Day/dia 11.py
@@ -0,0 +1,12 @@
+def addTwoNumbres ():
+    c = 2
+    b = 4
+    a = b + c
+    print(a)
+addTwoNumbres ()
+def areaCirculo ():
+    radio = 12
+    pi = 3.1416
+    area = pi * radio **2
+    print(area)
+areaCirculo ()
