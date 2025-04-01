4a029ff486a9dccd329735f0ee946cbafa90091e
javier <up240029@alumnos.upa.edu.mx>
2025-03-31 20:12:01 -0600

    Inicio dia 12

--- 12_Day/dia 12.py
+++ 12_Day/dia 12.py
@@ -1,2 +1,24 @@
 #Nivel 1 
 #1
+import random
+import string
+def randomUserId ():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k= 6))
+print('Este es su usuario ', randomUserId())
+#2
+def userIdGenByUser ():
+    noCar = int(input('Ingrese el numero de caracteres '))
+    noIds = int(input('ingrese el numero de ids '))
+    Ids = [
+        ''.join(random.choices(string.ascii_letters + string.digits, k= noCar))
+        for _ in range(noIds)
+    ]
+    return Ids
+print(userIdGenByUser())
+#3
+def genRgbColor ():
+    rojo = random.randint(0,255)
+    azul = random.randint(0,255)
+    verde = random.randint(0,255)
+    return(rojo,azul,verde)
+print(genRgbColor())
