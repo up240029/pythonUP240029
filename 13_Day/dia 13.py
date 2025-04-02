317b39823e3c77ce820ae3786b54d2fb12dec936
javier <up240029@alumnos.upa.edu.mx>
2025-04-01 22:11:59 -0600

    fin dia 13

--- 13_Day/dia 13.py
+++ 13_Day/dia 13.py
@@ -9,3 +9,23 @@ print(listaAplanada)
 #3
 tupleList = [(i, 1, i, i**2, i**3, i**4, i**5)for i in range (11)] 
 print(tupleList)
+#4
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+lista = [i for i in countries for j in i for i in j]
+lista = [[i.upper()]for i in lista]
+print(lista)
+#5
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+dicCountries = [{'country':country.upper(), 'city' : city.upper() } for [(country,city )]in countries]
+print(dicCountries)
+#6
+names = [[('Asabeneh', 'Yetayeh')], [('David', 'Smith')], [('Donald', 'Trump')], [('Bill', 'Gates')]]
+names = [''.join(i)for j in names for i in j] 
+print(names )
+#7
+slope = lambda x1, y1, x2, y2 :(y2-y1) / (x2-x1)
+yIntercept = lambda x1, y1, m: y1 - (m * x1)
+m = slope(2,3,5,7)
+print(m)
+print(yIntercept(2,3,m))
+# No se si este este bien hecho pero no supe muy bien como hacerlo 
